6bcf972e45cd3b9f5f31ed256e8c07553e73fa37
Ibrahim Mohamed <109541964+Ibrahim11511@users.noreply.github.com>
2023-09-17 16:38:41 +0300

    Add files via upload

---
+++ Assignment 9/script.js
@@ -0,0 +1,12 @@
+let first_name = document.querySelector(".f-name");
+let second_name = document.querySelector(".s-name");
+let user_age = document.querySelector(".age");
+let container = document.querySelector(".perant");
+
+
+first_name.textContent = "Ibrahim";
+second_name.textContent = "Mohamed";
+user_age.textContent = "45";
+container.style.backgroundColor = "blue";
+
+console.log('Done');
